b149edcdf44111ee3c7e185e5302569932dea7af
mamun-0 <md.abdullahalmamun200@gmail.com>
2021-06-15 14:37:01 +0600

    Ready to upload

---
+++ src/Components/Notice-Grid/noticeData.js
@@ -0,0 +1,14 @@
+const NOTICE_DATA = [
+  {
+    id: 1,
+    noticeHeading: "Assignment (DLD-253) | 2-2",
+    noticeDetails:
+      "An assignment is given to us by Departmental Chairman Dr. Saleh Ahmed Sir. You should complete and submit it to 'dld.sumon.edu@gmail.com' before the deadline. Assignment topics are discribed in PDF file.",
+    downloadLink:
+      "https://ln4.sync.com/dl/c36a28750/2deg6peu-xsewwpss-akduc9a5-fejtsdpg",
+    a_tag_title: "Download PDF",
+    publishedDate: "Published Date: 15/06/2021",
+    deadLine: "Submission Last Date : 25/06/2021",
+  },
+];
+export default NOTICE_DATA;
---
+++ src/Components/Notice/notice.component.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./notice.style.css";
+const NoticeBoard = (props) =>
+  props.noticeInformation.map((item) => (
+    <div key={item.id} className='notice-wrap'>
+      <div className='notice--card'>
+        <h2 className='notice__heading notice__heading__small'>
+          {item.noticeHeading}
+        </h2>
+        <p className='notice__discription nospace'>{item.noticeDetails}</p>
+        <a className='btn' href={item.downloadLink}>
+          {item.a_tag_title}
+        </a>
+        <p className='notice__published nospace'>{item.publishedDate}</p>
+        <p className='notice__published nospace'>{item.deadLine}</p>
+      </div>
+    </div>
+  ));
+export default NoticeBoard;
