dd9b0bf5315e2adfa1351312c638a9052e85f3d0
mamun-0 <md.abdullahalmamun200@gmail.com>
2021-06-18 00:15:56 +0600

    Update PDF files and video Card

--- src/Components/Syllabus/pdfData.js
+++ src/Components/Syllabus/pdfData.js
@@ -7,6 +7,20 @@ const PDF_DATA = [
       "https://drive.google.com/file/d/14O8LJX_5XlKEmDANc1Yr4O9hNBiOIXjH/view",
   },
   {
+    id: 16,
+    title: "Assignment Cover Page",
+    a_tag_title: "Download",
+    downloadLink:
+      "https://ln4.sync.com/dl/2f2274f30/ufpkdunf-xcviukq3-fxbabj9y-879ht79k",
+  },
+  {
+    id: 17,
+    title: "Lab Report Cover Page",
+    a_tag_title: "Download",
+    downloadLink:
+      "https://ln4.sync.com/dl/2003d4620/qwhkbj95-efv8mugi-hi89yrmw-sf39tak2",
+  },
+  {
     id: 2,
     title: "Previous Year Questions",
     a_tag_title: "Download",
@@ -40,7 +54,7 @@ const PDF_DATA = [
   },
   {
     id: 7,
-    title: "Statistics All PDF",
+    title: "Statistics All PDF | 2-1",
     a_tag_title: "Visit",
     downloadLink:
       "https://drive.google.com/drive/folders/1r-mLSlPXGWW-yejtJhhwhdDV0qK_fnbz",
@@ -54,10 +68,54 @@ const PDF_DATA = [
   },
   {
     id: 9,
-    title: "Vector-Analysis (Math 2-1)",
+    title: "Vector-Analysis (Math) | 2-1",
     a_tag_title: "Download",
     downloadLink:
       "https://drive.google.com/file/d/1NayNih8fRWmd-6z_UVL7TKILAbh7EgMf/view?usp=sharing",
+  },
+  {
+    id: 14,
+    title: " Proteus 8 with Crack(203) | 2-1",
+    a_tag_title: "Download",
+    downloadLink:
+      "https://drive.google.com/drive/folders/1QqmbbMONUrgYoGaMx7LeRRtizmHu3yxd?usp=sharing",
+  },
+  {
+    id: 15,
+    title:
+      "Digital Logic Design[All Files Included Except Proteus] - 203 | 2-1",
+    a_tag_title: "Download",
+    downloadLink:
+      "https://drive.google.com/file/d/1uZjnNsshMg4ygEjchPPWbTyedvmtDIO7/view?usp=sharing",
+  },
+  {
+    id: 10,
+    title: "Theory OF Computing (255) | 2-2",
+    a_tag_title: "Download",
+    downloadLink:
+      "https://drive.google.com/file/d/1q_4AwqITFHw2i7YQ69dR3ib7SLAXpEGz/view?usp=sharing",
+  },
+  {
+    id: 11,
+    title: "Introduction To Digital Systems (253)  | 2-2",
+    a_tag_title: "Download",
+    downloadLink:
+      "https://drive.google.com/file/d/1-RqFKT16qy2AaXkpGvQENFGFHNDNJS6c/view?usp=sharing",
+  },
+  {
+    id: 12,
+    title: "Algorithms Design And Analysis (251) | 2-2",
+    a_tag_title: "Download",
+    downloadLink:
+      "https://drive.google.com/file/d/1qVDbIy2MtVP1nnSg4aTlpGOYlLhpF-oB/view?usp=sharing",
+  },
+  {
+    id: 13,
+    title:
+      "Digital Systems Principles And Applications PDF [Ronald Tocci]| 2-2",
+    a_tag_title: "Download",
+    downloadLink:
+      "https://drive.google.com/file/d/1aRiVmRW_bUDF3MEV-_aiKdFf5s6RIXCT/view?fbclid=IwAR0y0pB6CbwVvGajiH25UcV1Dsi-J6MygB_mp-mcfQ35tuv9nHfJw1NUb2I",
   },
 ];
 export default PDF_DATA;
--- src/pages/Online-Classes/online-classes.component.jsx
+++
@@ -1,4 +0,0 @@
-import React from "react";
-import OnlineClasses from "";
-const Covid19 = () => <OnlineClasses />;
-export default Covid19;
